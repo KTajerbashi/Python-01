33fc1ac2a9595931b6ca9362713ed97a4c3798f1
Tajerbashi <kamrantajerbashi@gmail.com>
2023-02-22 00:31:29 +0330

    Cook Program is completed

--- Cooking.py
+++ Cooking.py
@@ -1,4 +1,15 @@
-i = 0
+i = 1
 m = 0
+lastNumber = 0
 
 m = int(input("M : "))
+while m != -1:
+    if i % 2 == 0:
+        lastNumber += m
+    else:
+        lastNumber -= m
+    
+    i += 1
+    m = int(input("M : "))
+    
+print("Result : ", lastNumber)
